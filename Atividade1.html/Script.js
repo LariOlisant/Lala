785f8f8e7e3908e7a5664741316449fb306f1c75
LariOlisant <larissa.o.santana@aluno.senai.br>
2025-04-03 09:55:50 -0300

    Exercício 2 DOM

--- Atividade1.html/Script.js
+++ Atividade1.html/Script.js
@@ -1,19 +1,49 @@
 
-        // Selecionando elementos de diferentes formas
-        let titulo = document.getElementById("titulo");
-        console.log("Selecionado por ID:", titulo);
+// Selecionando elementos de diferentes formas
+let titulo = document.getElementById("titulo");
+console.log("Selecionado por ID:", titulo.textContent);
 
-        let esportesPorClasse = document.getElementsByClassName("esporte");
-        console.log("Selecionados por Classe:", esportesPorClasse);
+let esportesPorClasse = document.getElementsByClassName("esporte");
+console.log("Selecionados por Classe:", esportesPorClasse[0].textContent);
+console.log("Selecionados por Classe:", esportesPorClasse[1].textContent);
+console.log("Selecionados por Classe:", esportesPorClasse[2].textContent);
+console.log("Selecionados por Classe:", esportesPorClasse[3].textContent);
+console.log("Selecionados por Classe:", esportesPorClasse[4].textContent);
+console.log("Selecionados por Classe:", esportesPorClasse[5].textContent);
+console.log("Selecionados por Classe:", esportesPorClasse[6].textContent);
+console.log("Selecionados por Classe:", esportesPorClasse[7].textContent);
+console.log("Selecionados por Classe:", esportesPorClasse[8].textContent);
 
-        let esportesPorTag = document.getElementsByTagName("li");
-        console.log("Selecionados por Tag:", esportesPorTag);
 
-        let primeiroEsporte = document.querySelector(".esporte");
-        console.log("Selecionado pelo querySelector:", primeiroEsporte);
 
-        let todosEsportes = document.querySelectorAll(".esporte");
-        console.log("Selecionados pelo querySelectorAll:", todosEsportes);
+let esportesPorTag = document.getElementsByTagName("li");
+console.log("Selecionados por Tag:", esportesPorTag[0]);
 
-        // Modificando a cor do título
-        titulo.style.color = "#fff";
+let primeiroEsporte = document.querySelector(".esporte");
+console.log("Selecionado pelo querySelector:", primeiroEsporte);
+
+let todosEsportes = document.querySelectorAll(".esporte");
+console.log("Selecionados pelo querySelectorAll:", todosEsportes);
+
+// Modificando a cor do título
+titulo.style.color = "#fff";
+let lista = document.getElementById("lista-esporte");
+
+let novoEsporte = document.createElement("li");
+novoEsporte.innerText = "Beach Tennis";
+
+//Adicionando o novo parágrafo ao body com appendChild
+lista.appendChild(novoEsporte);
+
+
+let novoEsporte2 = document.createElement("li");
+novoEsporte2.innerText = "Elefante Polo🐘";
+
+//Adicionando o novo parágrafo ao body com appendChild
+lista.appendChild(novoEsporte2);
+
+let novoEsporte3 = document.createElement("li");
+novoEsporte3.innerText = "Luta de sabre de luz✨";
+
+//Adicionando o novo parágrafo ao body com appendChild
+lista.appendChild(novoEsporte3);
--- Script.js
+++ Script.js
@@ -1,19 +1,40 @@
-//Busacndo elementos por ID
-let titulo = document.getElementById("titulo");
-console.log(titulo);
+//Buscando elementos por ID
+// let titulo = document.getElementById("titulo");
+// console.log(titulo);
 
-//Buscando elementos por clase
-let descricao = document.getElementsByClassName("descricao");
-console.log(descricao[0]);
+// //Buscando elementos por clase
+// let descricao = document.getElementsByClassName("descricao");
+// console.log(descricao[0]);
 
-//Selecionando pelo nome da TAG
-let paragrafo = document.getElementsByTagName("p");
-console.log(paragrafo[0])
+// //Selecionando pelo nome da TAG
+// let paragrafo = document.getElementsByTagName("p");
+// console.log(paragrafo[0])
 
-// Selecionando pelo Query Selector
-// # significa Id -> o nome do ID é titulo
-// "." significa classe -> o nome da classe é descrição
-let tituloQS = document.querySelector("#titulo");
-let paragrafoQS = document.querySelector(".descricao");
-console.log("Exibindo com Query Selector" + tituloQS);
-console.log("Exibindo com QS o paragrago" + paragrafoQS[0].textContent);
+// // Selecionando pelo Query Selector
+// // # significa Id -> o nome do ID é titulo
+// // "." significa classe -> o nome da classe é descrição
+// let tituloQS = document.querySelector("#titulo");
+// let paragrafoQS = document.querySelector(".descricao");
+// console.log("Exibindo com Query Selector" + tituloQS);
+// console.log("Exibindo com QS o paragrago" + paragrafoQS[0].textContent);
+
+//Alterando o style de um elemento
+titulo.innerText = "Novo Título"
+titulo.innerHTML = "<span style='color:red'> Título em vermelho </span>"
+
+//Alterando o style de um elemento com JS
+titulo.style.backgroundColor = "black"; //Fundo preto
+titulo.style.fontSize = "50px"; //Aumenta o tamanho da fonte
+
+//Adicionando um novo elemento
+let novoParagrafo = document.createElement("p");
+novoParagrafo.innerText = "Este é um novo parágrafo!";
+
+//Adicionando o novo parágrafo ao body com appendChild
+document.body.appendChild(novoParagrafo);
+
+//Removendo elementos do DOM com JS removeChild
+let elementoParaRemover = document.querySelector(".descricao");
+document.body.removeChild(elementoParaRemover);
+
+
